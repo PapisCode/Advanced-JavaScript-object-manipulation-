384bb7527c4c184bcca0bca7174335c017bedf8b
Gabriel Hernandez <gabhernandez@hotmail.ca>
2024-11-26 23:02:12 -0500

    Completed console log input, fixed bug errors, texted script - completed part 1

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // Part 1: Student Information
 
-const students = {
+const student = {
         name: " G Hernandez",
         age : 30,
         enrolled: true,
         courses: ["Fundamentals of JavaScript", "Algorithm and Problem Solving", "Database Concept and Design"],
         displayInfo() {
             return '${this.name} is ${this.age} years old.';
-        }
-    }
+        },
+        addCourse(course) {
+            this.courses.push(course);
+        },
+        totalCourses() {
+            return this.courses.length;
+        },
+    };
+
+    // Console.log output
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.courses);
+console.log(student.displayInfo());
