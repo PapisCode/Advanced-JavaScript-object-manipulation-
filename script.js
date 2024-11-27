ba434f44b1edff9dee0117af0e4efb9d5f94833c
Gabriel Hernandez <gabhernandez@hotmail.ca>
2024-11-26 22:50:12 -0500

    Added course names and started the method to display the students info

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Part 1: Student Information
 
 const students = {
-    const student = {
-        name: " G Hernandez"
-        age : 30
+        name: " G Hernandez",
+        age : 30,
         enrolled: true,
+        courses: ["Fundamentals of JavaScript", "Algorithm and Problem Solving", "Database Concept and Design"],
+        displayInfo() {
+            return '${this.name} is ${this.age} years old.';
+        }
     }
-};
